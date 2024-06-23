db46b6926fa1d8f0673fc004774a57b6fe4557cd
m2hs4n <m2hs4n@gmail.com>
2024-06-24 01:14:37 +0330

    feat: Transaction view

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -7,6 +7,21 @@ from card.models import Card
 from transaction.models import Transaction
 
 
+class TransactionSerializer(serializers.Serializer):
+    sender = serializers.CharField()
+    receiver = serializers.CharField()
+    amount = serializers.IntegerField()
+
+    def validate(self, value):
+        if not value["sender"] == value["receiver"]:
+            if Card.objects.filter(Q(card_number=value["sender"]) | Q(card_number=value["receiver"])).count() == 2:
+                if Card.objects.get(card_number=value["sender"]).stock >= value["amount"]:
+                    return value
+                raise serializers.ValidationError({"message": "Don't have enough money"})
+            raise serializers.ValidationError({"message": "Invalid sender or receiver"})
+        raise serializers.ValidationError({"message": "You can't transaction to your self"})
+
+
 class TransactionListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
--- transaction/views.py
+++ transaction/views.py
@@ -12,3 +12,23 @@ from transaction.models import Transaction
 class TransactionListView(generics.ListAPIView):
     serializer_class = serializers.TransactionListSerializer
     queryset = Transaction.objects.all()
+
+
+class TransactionView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = serializers.TransactionSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                with transaction.atomic():
+                    sender = Card.objects.get(card_number=serializer.data['sender'])
+                    receiver = Card.objects.get(card_number=serializer.data['receiver'])
+                    sender.stock -= serializer.data['amount']
+                    receiver.stock += serializer.data['amount']
+                    trans = Transaction(sender=sender, receiver=receiver, amount=serializer.data['amount'])
+                    trans.save()
+                    sender.save()
+                    receiver.save()
+                    return Response(data={"message": "transaction successfully"}, status=status.HTTP_201_CREATED)
+            except Exception as e:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
