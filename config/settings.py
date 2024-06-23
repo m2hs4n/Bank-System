2736caf2170fe4df84352c8c370026306d72ea4b
m2hs4n <m2hs4n@gmail.com>
2024-06-23 19:50:56 +0330

    feat: Setting up connection to postgres database

--- config/settings.py
+++ config/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'debug_db',
+        'USER': 'debug',
+        'PASSWORD': 'debug',
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
 
