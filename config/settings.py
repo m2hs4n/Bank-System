3fb8eb754d75984fe46b35a2d8d831ae40df2b3a
m2hs4n <m2hs4n@gmail.com>
2024-06-24 16:24:42 +0330

    fix: Authentication

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 SPECTACULAR_SETTINGS = {
